cb147872fbef3c8536c2acd6b300561b7fe315e2
Håvard Opheim <haavard@vaage.com>
2023-05-23 17:57:42 +0200

    add podcast support

--- schemas/schema.ts
+++ schemas/schema.ts
@@ -3,6 +3,16 @@ import category from "./category";
 import post from "./post";
 import author from "./author";
 import mjukvare from "./mjukvare";
+import episode from "./episode";
+import podcast from "./podcast";
 
 // Then we give our schema to the builder and provide the result to Sanity
-export default [mjukvare, post, author, category, blockContent];
+export default [
+  mjukvare,
+  post,
+  author,
+  category,
+  blockContent,
+  episode,
+  podcast,
+];
